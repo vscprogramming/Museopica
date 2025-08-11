6e56f0989cf10290c1a3616f028260bb3a191f14
shin-programming <nishikawa-s-23-29ks@g.bears.ed.jp>
2025-08-11 21:06:26 +0900

    v0.5

--- js/resize_window.js
+++ js/resize_window.js
@@ -4,6 +4,7 @@ function resize_canvas(all_canvas) {
     let c = 0;
 
     all_canvas.forEach(canvas => {
+        console.log(canvas);
         c++;
         const context = canvas.getContext('2d');
 
@@ -21,6 +22,24 @@ function resize_canvas(all_canvas) {
         canvas.width = width;
         canvas.height = height;
 
+        // エディタの領域（縦）
+        let editor_height_start = height * 0.04; // エディタの開始位置
+        let editor_height_end = height * 0.96; // エディタの終了位置
+        let editor_height = editor_height_end - editor_height_start; // エディタの高さ
+
+        // 鍵盤の大きさ
+        let white_keyboard_height = editor_height / (36 + 9); // 白鍵の高さ（白鍵36+ドラム鍵盤9）
+        let white_keyboard_width = white_keyboard_height * 7; // 白鍵の幅
+        let black_keyboard_height = white_keyboard_height * 8 / 14; // 黒鍵の高さ
+        let black_keyboard_width = black_keyboard_height * 8; // 黒鍵の幅
+
+        // エディタの領域（横）
+        let editor_width_start = white_keyboard_width; // エディタの開始位置
+        let editor_width_end = width - 1; // エディタの終了位置
+        let editor_width = editor_width_end - editor_width_start; // エディタの幅
+
+        let quarterbeat_width = editor_width / 64;  // 0.25拍の横幅
+
         // 背景の描画(background)
         if (c == 1) {
             // 塗りつぶし
@@ -28,36 +47,18 @@ function resize_canvas(all_canvas) {
             context.fillRect(0, 0, width, height);
 
             // UI背景の描画
-            //context.fillStyle = '#0000005e';
+            // context.fillStyle = '#0000005e';
             context.fillRect(0, 0, width, height * 0.04);
             context.fillRect(0, height * 0.96, width, height);
-
-            // エディタの領域（縦）
-            let editor_height_start = height * 0.04; // エディタの開始位置
-            let editor_height_end = height * 0.96; // エディタの終了位置
-            let editor_height = editor_height_end - editor_height_start; // エディタの高さ
-
-            // 鍵盤の大きさ
-            let white_keyboard_height = editor_height / (35 + 9); // 白鍵の高さ（白鍵35+ドラム鍵盤9）
-            let white_keyboard_width = white_keyboard_height * 7; // 白鍵の幅
-            let black_keyboard_height = white_keyboard_height * 8 / 14; // 黒鍵の高さ
-            let black_keyboard_width = black_keyboard_height * 8; // 黒鍵の幅
-
-            // エディタの領域（横）
-            let editor_width_start = white_keyboard_width; // エディタの開始位置
-            let editor_width_end = width; // エディタの終了位置
-            let editor_width = editor_width_end - editor_width_start; // エディタの幅
-
-            let quarterbeat_width = editor_width / 64;  // 0.25拍の横幅
 
             // 鍵盤・線の描画
 
             // 通常線
             context.strokeStyle = '#828282';
-            context.lineWidth = 0.25;
+            context.lineWidth = 0.5;
             context.beginPath();
 
-            for (let i = 0; i <= 35 + 9; i++) {
+            for (let i = 0; i <= 36 + 9; i++) {
                 // 横線
                 context.moveTo(editor_width_start, editor_height_start + i * white_keyboard_height);
                 context.lineTo(editor_width_end, editor_height_start + i * white_keyboard_height);
@@ -74,28 +75,82 @@ function resize_canvas(all_canvas) {
             context.stroke();
 
             // 縦：1拍ごと
-            context.lineWidth = 0.5;
+            context.lineWidth = 1;
             context.beginPath();
 
             for (let i = 1; i <= 64; i++) {
-                // 縦線
-                if (i % 4 == 0) {
+                if (i % 4 == 0 && !(i % 16 == 0)) {
                     context.moveTo(editor_width_start + i * quarterbeat_width, editor_height_start);
                     context.lineTo(editor_width_start + i * quarterbeat_width, editor_height_end);
                 };
             };
 
             context.stroke();
+
+            // 縦：4拍ごと
+            context.lineWidth = 2;
+            context.strokeStyle = '#0008ff';
+            context.beginPath();
+
+            for (let i = 1; i <= 64; i++) {
+                if (i % 16 == 0) {
+                    context.moveTo(editor_width_start + i * quarterbeat_width, editor_height_start);
+                    context.lineTo(editor_width_start + i * quarterbeat_width, editor_height_end);
+                };
+            };
+
+            context.stroke();
+
+            // 横：オクターブ間（白鍵）
+            context.lineWidth = 1;
+            context.strokeStyle = '#cc0000';
+            context.beginPath();
+
+            for (let i = 0; i < 36; i++) {
+                if (i % 7 == 1 || i % 7 == 5) {
+                    context.moveTo(editor_width_start, editor_height_start + i * white_keyboard_height);
+                    context.lineTo(editor_width_end, editor_height_start + i * white_keyboard_height);
+                };
+            };
+
+            context.stroke();
+
+            // ドラム境目
+            context.lineWidth = 1;
+            context.strokeStyle = '#4acc00';
+            context.beginPath();
+            context.moveTo(editor_width_start, editor_height_start + 36 * white_keyboard_height);
+            context.lineTo(editor_width_end, editor_height_start + 36 * white_keyboard_height);
+            context.stroke();
         };
 
         // 白鍵盤・UIの描画(Wkey_ui)
         if (c == 2) {
+            // 鍵盤の描画
+            for (let i = 0; i < 36 + 9; i++) {
+                context.fillStyle = '#fff';
+                context.fillRect(0, editor_height_start + i * white_keyboard_height, white_keyboard_width, white_keyboard_height);
+            };
+        };
+
+        // 黒鍵盤の描画(Bkey)
+        if (c == 3) {
 
         };
 
-        // 黒鍵盤・白鍵盤間の線の描画(Bkey)
-        if (c == 3) {
+        // 白鍵盤間の線・鍵盤上の文字・画像表示
+        if (c == 4) {
+            // 黒線
+            context.strokeStyle = '#000';
+            context.lineWidth = 0.5;
+            context.beginPath();
 
+            for (let i = 1; i < 36 + 9; i++) {
+                context.moveTo(0, editor_height_start + i * white_keyboard_height);
+                context.lineTo(white_keyboard_width, editor_height_start + i * white_keyboard_height);
+            };
+
+            context.stroke();
         };
 
         const app = Object.assign(document.getElementById('app').style, {
