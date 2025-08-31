ecaee6369d7e73066a28d65a8f2a9326d9dcf8e5
shin-programming <nishikawa-s-23-29ks@g.bears.ed.jp>
2025-08-31 17:52:02 +0900

    v0.7

--- js/main.js
+++ js/main.js
@@ -1,102 +1,37 @@
 // 起動処理（タイトルと楽器の読み込み処理）
-const button = document.getElementById('button');
-
-let instrument = [];
 let drum = null;
-let audio_ctx = null;
-
-const instrument_view = new Map([
-    [1, 'bright_acoustic_piano'],
-    [2, 'drawbar_organ'],
-    [3, 'acoustic_guitar_nylon'],
-    [4, 'electric_guitar_clean'],
-    [5, 'electric_bass_finger'],
-    [6, 'pan_flute'],
-    [7, 'vibraphone'],
-    [8, 'marimba'],
-    [9, 'voice_oohs'],
-    [10, 'lead_1_square'],
-    [11, 'lead_8_bass__lead'],
-]);
 
 document.getElementById('start_display').addEventListener('click', async () => {
     document.getElementById('start_display').innerHTML = `
         <h1>Museopica</h1>
         <h6>読み込み中...</h6>
+        <p>v0.6</p>
     `;
 
     if (!audio_ctx) {
         audio_ctx = new AudioContext();
         await audio_ctx.resume();
-    };
+    }
 
-    for (let c = 1; c < 12; c++) {
-        instrument[c] = await Soundfont.instrument(audio_ctx, `${instrument_view.get(c)}`);
-    };
-
+    for (let c = 1; c < 12; c++) instrument[c] = await Soundfont.instrument(audio_ctx, `${instrument_view.get(c)}`);
     console.log(instrument);
 
     document.getElementById('start_display').innerHTML = `
         <h1>Museopica</h1>
         <h6>完了！</h6>
+        <p>v0.6</p>
     `;
 
+    console.log('instrument');
+    console.log(instrument);
+
     setTimeout(() => {
-        document.getElementById('start_display').classList.add('clicked'); // タイトル画面フェードアウト
+        document.getElementById('start_display').classList.add('clicked');
     }, 2000);
 
     setTimeout(() => {
         document.getElementById('start_display').style.zIndex = -9999;
     }, 4000);
-});
-
-let bpm = 60; // テンポ
-let SPB = 60 / bpm; // 1拍の長さ（s）
-let instrument_number = 1;  // 楽器番号（1~11）
-let p = 1; //仮
-
-// 再生テスト処理
-button.addEventListener('click', async () => {
-    let c = 60 - 0;
-
-    for (let i = c; i <= c; i++) {
-
-        if (p == 0) {
-            instrument[instrument_number].play(i - 24, audio_ctx.currentTime, { duration: SPB, gain: 1 });
-            //instrument.play(i + 12, audio_ctx.currentTime, { duration: SPB });
-        } else if (p == 1) {
-            instrument[instrument_number].play(i, audio_ctx.currentTime, { duration: SPB, gain: 1 });
-            instrument[instrument_number].play(i + 4, audio_ctx.currentTime, { duration: SPB, gain: 1 });
-            instrument[instrument_number].play(i + 7, audio_ctx.currentTime, { duration: SPB, gain: 1 });
-        };
-        // duration = 拍数, gain = 音量
-    };
-});
-
-
-// テスト用
-document.getElementById('button2').addEventListener('click', () => {
-    if (instrument_number == 11) {
-        instrument_number = 11;
-    } else {
-        instrument_number++;
-    }
-});
-
-document.getElementById('button3').addEventListener('click', () => {
-    if (instrument_number == 1) {
-        instrument_number = 1;
-    } else {
-        instrument_number--;
-    }
-});
-
-document.getElementById('button4').addEventListener('click', () => {
-    if (p == 0) {
-        p = 1;
-    } else {
-        p = 0;
-    }
 });
 
 /*
--- js/resize_window.js
+++ js/resize_window.js
@@ -1,42 +1,38 @@
+var white_keyboards = { x: [], y: [], w: [], h: [], pitch: [] },
+    drum_keyboards = { x: [], y: [], w: [], h: [], pitch: [] },
+    black_keyboards = { x: [], y: [], w: [], h: [], pitch: [] };
+
 const all_canvas = document.querySelectorAll('canvas');
 
 function resize_canvas(all_canvas) {
     let c = 0;
 
     all_canvas.forEach(canvas => {
-        console.log(canvas);
         c++;
         const context = canvas.getContext('2d');
 
-        const window_width = window.innerWidth;
-        const window_height = window.innerHeight;
-
-        let width = window_width;
-        let height = window_width * 9 / 16;
+        const window_width = window.innerWidth, window_height = window.innerHeight;
+        let width = window_width, height = window_width * 9 / 16;
 
         if (height > window_height) {
             height = window_height;
             width = window_height * 16 / 9;
-        };
+        }
 
         canvas.width = width;
         canvas.height = height;
 
         // エディタの領域（縦）
-        let editor_height_start = height * 0.04; // エディタの開始位置
-        let editor_height_end = height * 0.96; // エディタの終了位置
-        let editor_height = editor_height_end - editor_height_start; // エディタの高さ
+        let editor_height_start = height * .04, editor_height_end = height * .96, // エディタの開始位置, エディタの終了位置
+            editor_height = editor_height_end - editor_height_start; // エディタの高さ
 
         // 鍵盤の大きさ
-        let white_keyboard_height = editor_height / (36 + 9); // 白鍵の高さ（白鍵36+ドラム鍵盤9）
-        let white_keyboard_width = white_keyboard_height * 7; // 白鍵の幅
-        let black_keyboard_height = white_keyboard_height * 8 / 14; // 黒鍵の高さ
-        let black_keyboard_width = black_keyboard_height * 8; // 黒鍵の幅
+        let white_keyboard_height = editor_height / 45, white_keyboard_width = white_keyboard_height * 7, // 白鍵の高さ（白鍵36+ドラム鍵盤9）, 白鍵の幅
+            black_keyboard_height = white_keyboard_height * 8 / 14, black_keyboard_width = black_keyboard_height * 8; // 黒鍵の高さ, 黒鍵の幅
 
         // エディタの領域（横）
-        let editor_width_start = white_keyboard_width; // エディタの開始位置
-        let editor_width_end = width - 1; // エディタの終了位置
-        let editor_width = editor_width_end - editor_width_start; // エディタの幅
+        let editor_width_start = white_keyboard_width, editor_width_end = width - 1, // エディタの開始位置, エディタの終了位置
+            editor_width = editor_width_end - editor_width_start; // エディタの幅
 
         let quarterbeat_width = editor_width / 64;  // 0.25拍の横幅
 
@@ -48,29 +44,29 @@ function resize_canvas(all_canvas) {
 
             // UI背景の描画
             context.fillStyle = '#0000005e';
-            context.fillRect(0, 0, width, height * 0.04);
-            context.fillRect(0, height * 0.96, width, height);
+            context.fillRect(0, 0, width, height * .04);
+            context.fillRect(0, height * .96, width, height);
 
             // 鍵盤・線の描画
 
             // 通常線
             context.strokeStyle = '#828282';
-            context.lineWidth = 0.5;
+            context.lineWidth = .5;
             context.beginPath();
 
-            for (let i = 0; i <= 36 + 9; i++) {
+            for (let i = 0; i <= 45; i++) {
                 // 横線
                 context.moveTo(editor_width_start, editor_height_start + (i * white_keyboard_height));
                 context.lineTo(editor_width_end, editor_height_start + (i * white_keyboard_height));
-            };
+            }
 
             for (let i = 1; i <= 64; i++) {
                 // 縦線
                 if (!(i % 4 == 0)) {
                     context.moveTo(editor_width_start + (i * quarterbeat_width), editor_height_start);
                     context.lineTo(editor_width_start + (i * quarterbeat_width), editor_height_end);
-                };
-            };
+                }
+            }
 
             context.stroke();
 
@@ -82,8 +78,8 @@ function resize_canvas(all_canvas) {
                 if (i % 4 == 0 && !(i % 16 == 0)) {
                     context.moveTo(editor_width_start + (i * quarterbeat_width), editor_height_start);
                     context.lineTo(editor_width_start + (i * quarterbeat_width), editor_height_end);
-                };
-            };
+                }
+            }
 
             context.stroke();
 
@@ -96,8 +92,8 @@ function resize_canvas(all_canvas) {
                 if (i % 16 == 0) {
                     context.moveTo(editor_width_start + (i * quarterbeat_width), editor_height_start);
                     context.lineTo(editor_width_start + (i * quarterbeat_width), editor_height_end);
-                };
-            };
+                }
+            }
 
             context.stroke();
 
@@ -110,8 +106,8 @@ function resize_canvas(all_canvas) {
                 if (i % 7 == 1 || i % 7 == 5) {
                     context.moveTo(editor_width_start, editor_height_start + (i * white_keyboard_height));
                     context.lineTo(editor_width_end, editor_height_start + (i * white_keyboard_height));
-                };
-            };
+                }
+            }
 
             context.stroke();
 
@@ -122,70 +118,95 @@ function resize_canvas(all_canvas) {
             context.moveTo(editor_width_start, editor_height_start + (36 * white_keyboard_height));
             context.lineTo(editor_width_end, editor_height_start + (36 * white_keyboard_height));
             context.stroke();
-        };
+        }
 
-        // 白鍵盤・UIの描画(Wkey_ui)
+        // 鍵盤・UIの描画(key_ui)
         if (c == 2) {
             // 鍵盤の描画
-            for (let i = 0; i < 36 + 9; i++) {
-                context.fillStyle = '#fff';
-                context.fillRect(0, editor_height_start + (i * white_keyboard_height), white_keyboard_width, white_keyboard_height);
-            };
-        };
+            let x, y, w, h;
 
-        // 黒鍵の描画(Bkey)
-        if (c == 3) {
+            for (let i = 0, pitch = 98; i < 36 + 9; i++) {
+                x = 0;
+                y = editor_height_start + (i * white_keyboard_height);
+                w = white_keyboard_width;
+                h = white_keyboard_height;
+
+                if ((i + 1) % 7 == 2 || (i + 1) % 7 == 6) {
+                    pitch--;
+                } else {
+                    pitch -= 2;
+                }
+
+                if (i <= 35) {
+                    white_keyboards.x[i] = x + black_keyboard_width;
+                    white_keyboards.y[i] = y;
+                    white_keyboards.w[i] = w - black_keyboard_width;
+                    white_keyboards.h[i] = h;
+                    white_keyboards.pitch[i] = pitch;
+                } else {
+                    drum_keyboards.x[i - 36] = x;
+                    drum_keyboards.y[i - 36] = y;
+                    drum_keyboards.w[i - 36] = w;
+                    drum_keyboards.h[i - 36] = h;
+                    drum_keyboards.pitch[i - 36] = i - 35;
+                }
+
+                context.fillStyle = '#fff';
+                context.fillRect(x, y, w, h);
+            }
+
             context.fillStyle = '#000';
-            for (let i = 1; i < 36; i++) {
-                if (i % 7 == 2) {
-                    // A#(ラ#)
-                    context.fillRect(0, editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2), black_keyboard_width, black_keyboard_height);
-                } else if (i % 7 == 3) {
-                    // G#(ソ#)
-                    context.fillRect(0, editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2), black_keyboard_width, black_keyboard_height);
-                } else if (i % 7 == 4) {
-                    // F#(ファ#)
-                    context.fillRect(0, editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2), black_keyboard_width, black_keyboard_height);
-                } else if (i % 7 == 6) {
-                    // D#(レ#)
-                    context.fillRect(0, editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2), black_keyboard_width, black_keyboard_height);
-                } else if (i % 7 == 0) {
-                    // C#(ド#)
-                    context.fillRect(0, editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2), black_keyboard_width, black_keyboard_height);
-                };
-            };
-        };
+
+            for (let i = 1, pitch = 97, c = 0; i < 36; i++) {
+                x = 0;
+                y = editor_height_start + (i * white_keyboard_height) - (black_keyboard_height / 2);
+                w = black_keyboard_width;
+                h = black_keyboard_height;
+
+                if (i % 7 == 2 || i % 7 == 6) {
+                    // A#(ラ#), D#(レ#)
+                    pitch -= 3;
+                    black_keyboards.x[c] = x;
+                    black_keyboards.y[c] = y;
+                    black_keyboards.w[c] = w;
+                    black_keyboards.h[c] = h;
+                    black_keyboards.pitch[c] = pitch;
+                    context.fillRect(x, y, w, h);
+                    c++;
+                } else if (i % 7 == 3 || i % 7 == 4 || i % 7 == 0) {
+                    // G#(ソ#), F#(ファ#), C#(ド#)
+                    pitch -= 2;
+                    black_keyboards.x[c] = x;
+                    black_keyboards.y[c] = y;
+                    black_keyboards.w[c] = w;
+                    black_keyboards.h[c] = h;
+                    black_keyboards.pitch[c] = pitch;
+                    context.fillRect(x, y, w, h);
+                    c++;
+                }
+            }
+        }
 
         // 白鍵盤間の線・鍵盤上の文字・画像表示
-        if (c == 4) {
+        if (c == 3) {
             // 黒線
             context.strokeStyle = '#000';
-            context.lineWidth = 0.5;
+            context.lineWidth = .5;
             context.beginPath();
 
             for (let i = 1; i < 36 + 9; i++) {
                 context.moveTo(0, editor_height_start + (i * white_keyboard_height));
                 context.lineTo(white_keyboard_width, editor_height_start + (i * white_keyboard_height));
-            };
+            }
 
             context.stroke();
-        };
+        }
 
         const app = Object.assign(document.getElementById('app').style, {
             width: width + 'px',
             height: height + 'px'
         });
     });
-};
+}
 
-
-
-
-
-window.addEventListener('resize', () => {
-    resize_canvas(all_canvas);
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-    resize_canvas(all_canvas);
-});
+['resize', 'DOMContentLoaded'].forEach(event => window.addEventListener(event, () => resize_canvas(all_canvas)));
