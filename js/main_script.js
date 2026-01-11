12499aabcd68203f25cc179e79219ad67dc9f2b8
shin-programming <nishikawa-s-23-29ks@g.bears.ed.jp>
2026-01-11 22:56:06 +0900

    v0.23

--- js/main_script.js
+++ js/main_script.js
@@ -4,24 +4,28 @@ let pitch_data;
 let inst_id, instruments = [], drums = [];
 let playing = false;
 let mouse_x, mouse_y;
-let black_notes_judge;
-let mode, current_instrument, pitch, duration, vol, color, tempo, key;
-let notes = 0, clone = 0, played_notes = 0, time = '0:00:00.000', hour, minute, second, beats = '4, 4', page = 1;
+let mode, current_instrument, key_type, pitch, crap, duration, vol, color, tempo = 100, key;
+let notes = 0, clone = 0, played_notes = 0, time = '0:00:00.000', hour, minute, second, page = 1, layer = 1;
 let start;
-let start_time, elapsed_time;
+let start_time_bar, start_time, elapsed_time_bar, elapsed_time;
 
-const notes_data = [], Nplayed = [], save_data = {
-    notes: new Array(99998),
-    page_options: {
-        tempo: tempo,
-        key: key,
-        change_time: 0,
-        reference: 4,
-        measure: 4,
-        displayed_measure: 4
-    },
-    var: 0.22
+const notes_data = [];
+const Nplayed = [];
+const page_options = Array.from({ length: 100 }, () => ({
+    tempo: 100,
+    key: 0,
+    change_time: 0,
+    reference: 4,
+    measure: 4,
+    displayed_measure: 4
+}));
+const Pchanged = new Array(page_options.length).fill(false);
+const save_data = {
+    notes: new Array(99999),
+    page_options: new Array(100),
+    var: 0.23
 };
+tempo_change();
 
 let x, y, w, h;
 const wkps = new Array(50); // white key points（白鍵y座標　配列）
@@ -159,7 +163,6 @@ async function top_UI_prepare() {
 
     current_instrument = instruments[0];
     inst_id = inst_data[0].js
-    console.log(instruments);
 
     // 打楽器読み込み
     await Promise.all(drum_data.map(async (drum, i) => {
@@ -179,12 +182,11 @@ async function top_UI_prepare() {
 
     // テンポ初期処理
     const BPM_input = document.getElementById('BPM_input');
-    tempo = Number(BPM_input.value);
+    BPM_input.value = tempo;
 
     // 拍数スライダー変更時処理
     const dur_input = document.getElementById('dur_input');
     const dur_label = document.getElementById('dur_label');
-
     dur_input.addEventListener('input', dur_update_slider_property);
     dur_update_slider_property();
 
@@ -193,18 +195,18 @@ async function top_UI_prepare() {
         const val_input = val_label / dur_input.max * 100;
         dur_label.textContent = `${val_label}拍`;
         dur_input.style.setProperty('--dur-value', val_input + '%');
-        duration = Number(((60 / tempo) * dur_input.value).toFixed(3));
+        crap = dur_input.value;
+        duration = Number(((60 / tempo) * crap).toFixed(3));
     }
 
     // 音量スライダー変更時処理
     const vol_input = document.getElementById('vol_input');
     const vol_label = document.getElementById('vol_label');
-
     vol_input.addEventListener('input', vol_update_slider_property);
     vol_update_slider_property();
 
     function vol_update_slider_property() {
-        const val_label = vol_input.value
+        const val_label = vol_input.value;
         const val_input = val_label / vol_input.max * 100;
         vol_label.textContent = `${val_label}%`;
         vol_input.style.setProperty('--vol-value', val_input + '%');
@@ -218,6 +220,7 @@ async function top_UI_prepare() {
 
     // BPMテキストボックス変更時処理（規制）
     BPM_input.addEventListener('change', () => {
+        if (playing) return;
         const BPM_min = Number(BPM_input.min);
         const BPM_max = Number(BPM_input.max);
         const BPM_val = Number(BPM_input.value);
@@ -225,8 +228,9 @@ async function top_UI_prepare() {
         if (BPM_val > BPM_max) BPM_input.value = BPM_max;
         tempo = Number(BPM_input.value);
         duration = Number(((60 / tempo) * dur_input.value).toFixed(3));
-        stop_timer_update();
         left_bottom_ui_update();
+        notes_data_update_tempo();
+        tempo_change();
     });
 
     // BPMボタンクリック時
@@ -236,23 +240,25 @@ async function top_UI_prepare() {
     BPM_plus.addEventListener('click', () => {
         const BPM_max = Number(BPM_input.max);
         const BPM_val = Number(BPM_input.value);
-        if (BPM_val >= BPM_max) return;
+        if (BPM_val >= BPM_max || playing) return;
         BPM_input.value++;
         tempo = Number(BPM_input.value);
         duration = Number(((60 / tempo) * dur_input.value).toFixed(3));
-        stop_timer_update();
         left_bottom_ui_update();
+        notes_data_update_tempo();
+        tempo_change();
     });
 
     BPM_minus.addEventListener('click', () => {
         const BPM_min = Number(BPM_input.min);
         const BPM_val = Number(BPM_input.value);
-        if (BPM_val <= BPM_min) return;
+        if (BPM_val <= BPM_min || playing) return;
         BPM_input.value--;
         tempo = Number(BPM_input.value);
         duration = Number(((60 / tempo) * dur_input.value).toFixed(3));
-        stop_timer_update();
         left_bottom_ui_update();
+        notes_data_update_tempo();
+        tempo_change();
     });
 
     // キーテキストボックス変更時処理（規制）
@@ -283,10 +289,12 @@ async function top_UI_prepare() {
 }
 
 function timer_start() {
+    start_time_bar = performance.now();
     start_time = performance.now();
 
     const timer = setInterval(() => {
-        elapsed_time = (((performance.now() - start_time) / 1000) + ((page - 1) * 60 / tempo * 16)).toFixed(3);
+        elapsed_time_bar = (performance.now() - start_time_bar) / 1000;
+        elapsed_time = (elapsed_time_bar + ((page - 1) * 60 / tempo * 16)).toFixed(3);
         hour = Math.floor(elapsed_time / 3600);
         minute = Math.floor(elapsed_time / 60) % 60;
         second = (elapsed_time % 60).toFixed(3);
@@ -295,57 +303,90 @@ function timer_start() {
 
         if (!playing) {
             clearInterval(timer);
-            stop_timer_update();
+            elapsed_time = 0, elapsed_time_bar = 0;
+            time = '0:00:00.000';
             left_bottom_ui_update();
             return;
         }
     }, 1000 / 60);
 }
 
-function stop_timer_update() {
-    elapsed_time = ((page - 1) * 60 / tempo * 16).toFixed(3);
-    hour = Math.floor(elapsed_time / 3600);
-    minute = Math.floor(elapsed_time / 60) % 60;
-    second = (elapsed_time % 60).toFixed(3);
-    time = `${hour}:${minute <= 9 ? `0${minute}` : minute}:${second <= 10 ? `0${second}` : second}`;
-}
-
 function play_music() {
-    if (!notes_data || !Nplayed || !playing) {
-        clearInterval(music_play_interval);
-        return;
-    }
-    let i = 0;
+    // 演奏前判断（再生中か、notes_dataが空配列またはすべてdeletedか）
+    move_bar();
+    if (notes_data.every(v => v === 'deleted' || v == null)) return;
+
     const music_play_interval = setInterval(() => {
         if (!playing) {
-            for (let i = 0; i > Nplayed.length; i++) {
-                if (notes_data[i] !== null) {
-                    Nplayed[i] = false;
+            for (let c = 0; c < Nplayed.length; c++) {
+                if (notes_data[c] !== 'deleted' && notes_data[c] !== null) {
+                    Nplayed[c] = false;
                 }
             }
-            console.log(Nplayed);
+
+            played_notes = Nplayed.filter(v => v === true).length;
             clearInterval(music_play_interval);
             return;
         }
 
-        if (i >= notes_data.length) i = 0;
+        for (let i = 0; i < notes_data.length; i++) {
+            if (notes_data[i].start <= elapsed_time && !Nplayed[i] && notes_data[i].page == page) {
+                if (String(notes_data[i].pitch).includes('D')) {
+                    // ドラム
+                    // console.log('drum played');
+                } else {
+                    // 白鍵 / 黒鍵
+                    notes_data[i].inst.play(Number(notes_data[i].pitch) + key, audio_context_inst.currentTime, {
+                        gain: Number(notes_data[i].vol),
+                        duration: Number(notes_data[i].dur)
+                    });
 
-        if (notes_data[i].start <= elapsed_time && !Nplayed[i]) {
-            if (String(notes_data[i].pitch).includes('D')) {
-                // ドラム
-            } else {
-                // 白鍵 / 黒鍵
-                notes_data[i].inst.play(Number(notes_data[i].pitch) + key, audio_context_inst.currentTime, {
-                    gain: Number(notes_data[i].vol),
-                    duration: Number(notes_data[i].dur)
-                });
-
-                Nplayed[i] = true;
+                    Nplayed[i] = true;
+                    // console.log('played');
+                }
             }
         }
 
-        i++;
+        played_notes = Nplayed.filter(v => v === true).length;
     }, 1000 / 240);
+}
+
+function move_bar() {
+    const page_input = document.getElementById('page_input');
+
+    const music_play_interval = setInterval(() => {
+        if (!playing) {
+            for (let c = 0; c < Pchanged.length; c++) Pchanged[c] = false;
+            clearInterval(music_play_interval);
+            return;
+        }
+
+        if (page_options[page - 1].change_time < Number(elapsed_time_bar) && !Pchanged[page - 1]) {
+            if (page == 100) {
+                playing = false;
+                return;
+            }
+            start_time_bar = performance.now();
+            Pchanged[page - 1] = true;
+            page_input.value++;
+            page = Number(page_input.value);
+            left_bottom_ui_update();
+            paint_notes_again();
+        }
+    }, 1000 / 60);
+}
+
+function notes_data_update_tempo() {
+    for (let i = 0; i < notes_data.length; i++) {
+        if (notes_data[i] == 'deleted' || notes_data[i] == null) return;
+        notes_data[i].dur = Number(((60 / tempo) * notes_data[i].crap).toFixed(3));
+        const wait_page_second = 60 / tempo * 16;
+        notes_data[i].start = ((notes_data[i].page - 1) * wait_page_second) + (xps.indexOf(Number(notes_data[i].pos.x))) * (60 / tempo / 8);
+    }
+}
+
+function tempo_change() {
+    for (let i = 0; i < page_options.length; i++) page_options[i].change_time = 60 / tempo * 16;
 }
 
 function bottom_UI_prepare() {
@@ -354,15 +395,19 @@ function bottom_UI_prepare() {
     const page_input = document.getElementById('page_input');
 
     page_input.addEventListener('change', () => {
+        if (playing) {
+            page_input.value = page;
+            return;
+        }
         const page_min = Number(page_input.min);
         const page_max = Number(page_input.max);
         const page_val = Number(page_input.value);
         if (page_val < page_min) page_input.value = page_min;
         if (page_val > page_max) page_input.value = page_max;
         page = Number(page_input.value);
-        stop_timer_update();
+
         left_bottom_ui_update();
-        // ページ移動ノーツ再描画処理
+        paint_notes_again();
     });
 
     // ページボタンクリック時
@@ -372,33 +417,47 @@ function bottom_UI_prepare() {
     page_right.addEventListener('click', () => {
         const page_max = Number(page_input.max);
         const page_val = Number(page_input.value);
-        if (page_val >= page_max) return;
+        if (page_val >= page_max || playing) return;
         page_input.value++;
         page = Number(page_input.value);
-        stop_timer_update();
+
         left_bottom_ui_update();
-        // ページ移動ノーツ再描画処理
+        paint_notes_again();
     });
 
     page_left.addEventListener('click', () => {
         const page_min = Number(page_input.min);
         const page_val = Number(page_input.value);
-        if (page_val <= page_min) return;
+        if (page_val <= page_min || playing) return;
         page_input.value--;
         page = Number(page_input.value);
-        stop_timer_update();
+
         left_bottom_ui_update();
-        // ページ移動ノーツ再描画処理
+        paint_notes_again();
     });
 }
 
 function left_bottom_ui_update() {
     const notes_label = document.getElementById('notes_label');
     const time_label = document.getElementById('time_label');
-    const beats_label = document.getElementById('beats_label');
+    // const beats_label = document.getElementById('beats_label');
     notes_label.textContent = `Notes: ${played_notes} / ${notes}`;
     time_label.textContent = `Time: ${time}`;
-    beats_label.textContent = `Beats: ${beats}`;
+    // beats_label.textContent = `Beats: ${beats}`;
+}
+
+function paint_notes_again() {
+    const canvas_put_notes = document.getElementById('put_notes');
+    const pn_ctx = canvas_put_notes.getContext('2d');
+    pn_ctx.clearRect(0, 0, canvas_put_notes.width, canvas_put_notes.height);
+    if (notes_data.every(v => v === 'deleted' || v == null)) return;
+
+    notes_data.forEach(notes => {
+        if (notes.page == page) {
+            pn_ctx.fillStyle = notes.notes_type == 'B' ? darken_color(notes.color, 34) + 'aa' : notes.color + 'aa';
+            pn_ctx.fillRect(notes.pos.x, notes.pos.y, notes.pos.w, notes.pos.h);
+        }
+    });
 }
 
 async function inst_load() {
@@ -705,6 +764,7 @@ function move_notes() {
     ['mousemove', 'scroll'].forEach(event_type => window.addEventListener(event_type, event => {
         const top_ui = document.getElementById('top_ui');
         const bottom_ui = document.getElementById('bottom_ui');
+
         if (mode != 'create' || top_ui.matches(':hover') || bottom_ui.matches(':hover') || playing) {
             mn_ctx.clearRect(0, 0, canvas_move_notes.width, canvas_move_notes.height);
             return;
@@ -731,29 +791,28 @@ function move_notes() {
         const Mbk_min = Math.min(...Mbkps);
         const Mdk_min = Math.min(...Mdkps);
         const Mxp_min = Math.min(...Mxps);
-
         x = xps[Mxps.indexOf(Mxp_min)];
         w = grid_width * beat;
         const wait_page_second = 60 / tempo * 16;
-        start = ((page - 1) * wait_page_second) + (Mxps.indexOf(Mxp_min)) * (60 / tempo / 8);   // 設置後、変更時は全体を書きなければならない
+        start = ((page - 1) * wait_page_second) + (Mxps.indexOf(Mxp_min)) * (60 / tempo / 8);
 
         if ((Mdk_min < Mwk_min) && (Mdk_min < Mbk_min)) {
             mn_ctx.fillStyle = color + 'aa';
             y = dkps[Mdkps.indexOf(Mdk_min)] - (grid_height / 2);
             h = grid_height;
-            black_notes_judge = false;
-            pitch = `D${pitch_data.drum[Mdkps.indexOf(Mdk_min)]}`;
+            key_type = 'D';
+            pitch = pitch_data.drum[Mdkps.indexOf(Mdk_min)];
         } else if (Mwk_min < Mbk_min) {
             mn_ctx.fillStyle = color + 'aa';
             y = wkps[Mwkps.indexOf(Mwk_min)] - (grid_height / 2);
             h = grid_height;
-            black_notes_judge = false;
+            key_type = 'W';
             pitch = pitch_data.white[Mwkps.indexOf(Mwk_min)] + key;
         } else {
             mn_ctx.fillStyle = darken_color(color, 34) + 'aa';
             y = bkps[Mbkps.indexOf(Mbk_min)] - (grid_height * 0.6 / 2);
             h = grid_height * 0.6;
-            black_notes_judge = true;
+            key_type = 'B';
             pitch = pitch_data.black[Mbkps.indexOf(Mbk_min)] + key;
         }
 
@@ -764,13 +823,10 @@ function move_notes() {
 
 function darken_color(hex, percent) {
     hex = hex.replace('#', '');
-
     let red = parseInt(hex.slice(0, 2), 16);
     let green = parseInt(hex.slice(2, 4), 16);
     let blue = parseInt(hex.slice(4, 6), 16);
-
     const factor = 1 - percent / 100;
-
     red = Math.round(red * factor);
     green = Math.round(green * factor);
     blue = Math.round(blue * factor);
@@ -789,7 +845,7 @@ function edit() {
     const pn_ctx = canvas_put_notes.getContext('2d');
 
     move_notes.addEventListener('click', () => {
-        if (playing == true) return;
+        if (playing) return;
 
         switch (mode) {
             case 'create':
@@ -804,19 +860,22 @@ function edit() {
                             h: h
                         },
                         inst: current_instrument,
+                        key_type: key_type,
                         pitch: pitch,
                         dur: duration,
+                        crap: crap,
                         start: start,
                         vol: vol,
                         color: color,
                         page: page,
+                        layer: layer
                     }
                 );
 
                 Nplayed.push(false);
 
                 // ノーツ描画処理
-                pn_ctx.fillStyle = black_notes_judge ? darken_color(color, 34) + 'aa' : pn_ctx.fillStyle = color + 'aa';
+                pn_ctx.fillStyle = key_type == 'B' ? darken_color(color, 34) + 'aa' : color + 'aa';
                 pn_ctx.fillRect(x, y, w, h);
                 clone++;
                 notes++;
@@ -833,7 +892,7 @@ function edit() {
                 return;
         }
 
-        left_bottom_ui_update()
+        left_bottom_ui_update();
         console.log(notes_data);
     });
 }
@@ -847,25 +906,25 @@ function viewer_debug() {
             notes_width：${Math.round(w)}
             notes_height：${Math.round(h)}
             inst：${current_instrument.name}
+            key_type：${key_type}
             pitch：${pitch}
+            crap：${crap}
             dur：${duration}s
             start：${Number(start).toFixed(3)}s
             vol：${vol}
             color：${color}
-            page：${page}
-
+            page：${page}/
             tempo：${tempo}
-            key：${key}
-
+            key：${key}/
             mode：${mode}
             playing：${playing}
             notes：${notes}
             clone：${clone}
             mouse_x：${Math.round(mouse_x)}
-            mouse_y：${Math.round(mouse_y)}
-            
-            start_time：${start_time}
-            elapsed_time：${elapsed_time}
+            mouse_y：${Math.round(mouse_y)}/
+            stime：${Number(start_time).toFixed(3)}, ${Number(start_time_bar).toFixed(3)}
+            etime：${Number(elapsed_time).toFixed(3)}, ${Number(elapsed_time_bar).toFixed(3)}
+            ctime：${page_options[page - 1].change_time.toFixed(3)}
             time：${time}`;
     }, 1000 / 60);
 }
